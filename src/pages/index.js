b739ca3b514c7bfea9f4156caf141a0272492f65
nolan-dyke <nolan.g.dyke@gmail.com>
2020-09-03 11:40:12 -0600

    changed favicon path

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react"
 import About from "../components/About"
 import Navbar from '../components/Navbar'
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 
 export default function index() {
   return (
     <>
+    <Helmet>
+      <title>Nolan Dyke</title>
+      <meta name="description" content="nolan dyke"/>
+    </Helmet>
       <Navbar />
       <div id="app">
         <p id="job-title">Web Developer / Software Engineer</p>
